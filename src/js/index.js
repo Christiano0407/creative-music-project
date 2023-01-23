e1da764fa74fb1f10dd4026cf81bfbdde0d36244
Christiano0407 <placebo0704@gmail.com>
2023-01-23 10:13:02 -0600

    add play music

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,49 @@
 //** === ========= Reproducer Music ==== =========== */
+//**! === All Music Information Data === */
+const musicData = [
+  {
+    backgroundImage: '../creative/img/plus-img.jpg',
+    posterUrl: '../creative/img/plus-img.jpg',
+    title: 'Happy Moments (Master)',
+    album: 'No Spirit',
+    year: 2022,
+    artist: 'No Spirit x Tonion',
+    musicPath: '../creative/music/assets_music_music-1.mp3',
+  },
+  {
+    backgroundImage: '../creative/img/plus-img1.jpg',
+    posterUrl: '../creative/img/plus-img1.jpg',
+    title: 'We Are Going To Be Ok (Master)',
+    album: 'No Spirit',
+    year: 2022,
+    artist: 'No Spirit x Jhove',
+    musicPath: '../creative/music/assets_music_music-2.mp3',
+  },
+  {
+    backgroundImage: '../creative/img/plus-img2.jpg',
+    posterUrl: '../creative/img/plus-img2.jpg',
+    title: 'Winter Meadow',
+    album: 'No Spirit',
+    year: 2022,
+    artist: 'No Spirit x  juniorodeo',
+    musicPath: '../creative/music/assets_music_music-3.mp3',
+  },
+  {
+    backgroundImage: '../creative/img/plus-img3.jpg',
+    posterUrl: '../creative/img/plus-img3.jpg',
+    title: 'From Where We Started',
+    album: 'No Spirit',
+    year: 2022,
+    artist: 'No Spirit',
+    musicPath: '../creative/music/assets_music_music-4.mp3',
+  },
+  {
+    backgroundImage: '../creative/img/01_3D.jpg',
+    posterUrl: '../creative/img/01_3D.jpg',
+    title: 'Where I Found You',
+    album: 'No Spirit',
+    year: 2022,
+    artist: 'No Spirit',
+    musicPath: '../creative/music/assets_music_music-5.mp3',
+  },
+];
